3898264b3f3c7470fb31b56fdf8646d98128d1ba
Rigved Rakshit <r.phate@gmail.com>
2021-12-04 15:52:01 -0500

    Added convenience script to produce the documentation

---
+++ generate_documentation.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+########################################################################################################################
+# What triggered that emotion? Emotion cause extraction in conversational discourse.
+# Copyright (C) 2021  Rigved Rakshit
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+########################################################################################################################
+
+# Create a temporary directory to store the modified code
+mkdir -p "code_doc"
+
+# Loop through every Python file
+for file in code/*.py; do
+  # If the file is empty or doesn't exist, move on to the next one
+  if [[ ! -s "${file}" ]]; then
+    continue
+  fi
+
+  # Strip the path and get the filename
+  file=$(basename "${file}")
+
+  echo "Preparing ${file} for doxygen..."
+
+  # Convert the Python code into doxygen supported Python code
+  doxypy "code/${file}" > "code_doc/${file}"
+done
+
+# Generate the documentation
+doxygen "emotion_discourse_doc.conf"
+
+# Remove the temporary code directory
+rm -r "code_doc"
